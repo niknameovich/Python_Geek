7b4bc8e464c0fa0ea71c5e82955f6e945f4c1992
niknameovich <nikcherneev@mail.ru>
2021-02-05 15:04:41 +0300

    delete recent

--- exercise6.py
+++
@@ -1,8 +0,0 @@
-startwith = float(input('отправной результат - '))
-target = float(input('желаемый результат -'))
-percent = float(input('введите интенсивность в процентах -'))
-daystotarget = 0;
-while startwith*(1+(percent/100)) < target:
-    daystotarget+=1
-    startwith = startwith*(1+(percent/100))
-print(f'необходимо затратить {daystotarget+1} дней')
